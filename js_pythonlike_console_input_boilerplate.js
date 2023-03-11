896af602b6ae544e12ce3e1268db194942401495
JIHO PARK <75483910+jhp0115@users.noreply.github.com>
2023-03-11 16:27:26 +0900

    refactor: js pythonlike input boilerplate

--- js_pythonlike_console_input_boilerplate.js
+++ js_pythonlike_console_input_boilerplate.js
@@ -1,6 +1,4 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
+const rl = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
@@ -8,34 +6,26 @@ const rl = readline.createInterface({
 const readlines = [];
 let input;
 
-rl.on('line', function (line) {
-  readlines.push(line);
-});
-rl.on('close', function () {
+rl.on('line', (line) => readlines.push(line));
+rl.on('close', () => {
   const input_generator = input_generator_constructor(readlines);
-  input = () => {
-    return input_generator.next().value;
-  };
+  input = () => input_generator.next().value;
   main();
   process.exit();
 });
 
 function* input_generator_constructor(readlines) {
-  for (const line of readlines) {
-    yield line;
-  }
+  for (const line of readlines) yield line;
 }
 
 // -----------[MAIN LOGIC HERE]---------------------------------------
 function main() {
-
   // input()함수를 사용하면 콘솔로 입력받은 라인을 한 줄 씩 문자열로 반환합니다.
 
   // --[vscode에서 입출력 예시 테스트하는 방법]------
   // 백준에서 입력 예시를 콘솔 창에 복사 붙여넣기하시고,
   // 엔터를 한 번 눌러서 마지막 줄까지 확실히 입력시킨 다음,
   // ctrl+C로 종료하시면 main() 함수가 실행됩니다.
-
 
   // 아래 예시는 백준 1181번 문제 풀이 코드입니다.
   const N = +input();
@@ -45,14 +35,8 @@ function main() {
     strings_set.add(input());
   }
 
-  [...strings_set]
-    .sort((a, b) => {
-      if (a.length === b.length) {
-        return a < b ? -1 : 1;
-      } else {
-        return a.length - b.length;
-      }
-    })
-    .forEach((str) => console.log(str));
-
+  const answer = [...strings_set]
+    .sort((a, b) => (a.length === b.length ? (a < b ? -1 : 1) : a.length - b.length))
+    .join('\n');
+  console.log(answer);
 }
