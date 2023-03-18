5fd0196b262f473bfcb9a32e1c8e3d968bd77a12
JIHO PARK <75483910+jhp0115@users.noreply.github.com>
2023-03-18 19:30:08 +0900

    백준 1620 나는야포켓몬마스터이다솜

---
+++ Baekjoon/S4_1620_나는야포켓몬마스터이다솜.js
@@ -0,0 +1,30 @@
+const platform = 0; // 0: Mine, 1: Baekjoon
+const filePath = platform === 1 ? '/dev/stdin' : './input.txt';
+const readlines = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const generator = input_generator_constructor(readlines);
+const input = () => generator.next().value;
+function* input_generator_constructor(readlines) {
+  for (const line of readlines) yield line;
+}
+main();
+
+// -----------[MAIN LOGIC]---------------------------------------
+function main() {
+  const [N, M] = input().split(' ').map(Number);
+  const number_name = new Map();
+  const name_number = new Map();
+  for (let i = 1; i <= N; i++) {
+    const name = input();
+    number_name.set(i, name);
+    name_number.set(name, i);
+  }
+
+  let result_string = '';
+  for (let i = 0; i < M; i++) {
+    const question = input();
+    result_string += Number.isNaN(+question)
+      ? name_number.get(question) + '\n'
+      : number_name.get(+question) + '\n';
+  }
+  console.log(result_string);
+}
