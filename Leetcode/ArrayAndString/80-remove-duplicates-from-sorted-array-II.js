9f80eb7082bf46cd2bfbdf3138ec4618499c343d
JIHO PARK <75483910+jhp0115@users.noreply.github.com>
2023-12-16 19:55:11 +0900

    Leetcode 80 without duplicating-counter

--- Leetcode/ArrayAndString/80-remove-duplicates-from-sorted-array-II.js
+++ Leetcode/ArrayAndString/80-remove-duplicates-from-sorted-array-II.js
@@ -3,22 +3,22 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+
+    /**
+    i: 순회용 포인터.
+    k: 목표에 부합하는 배열의 현재 길이, 원소를 새로 추가할 위치.
+
+    이미 추가된 구역의 원소들(k-2,k-1)과 새로운 원소(i)를 비교한다.
+    세 값을 비교하여 3연속 같은 값이 아니라면 nums[i]를 nums[k]에 복사한다.
+     */
     
-    let dupCnt = 1;
-    let k = 1;
+    let k = 2;
 
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i - 1] === nums[i]) {
-            dupCnt += 1;
-        } else {
-            dupCnt = 1;
-        }
-
-        if (dupCnt <= 2) {
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 1] || nums[i] !== nums[k - 2]) {
             nums[k] = nums[i];
             k += 1;
         }
-
 
     }
 
