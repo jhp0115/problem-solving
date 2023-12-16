129e51ee26c1dfe8d3729a2448f6ffa772cab9d1
JIHO PARK <75483910+jhp0115@users.noreply.github.com>
2023-12-16 19:22:47 +0900

    Leetcode 26 improve speed with index pointer

--- Leetcode/ArrayAndString/26-remove-duplicates-from-sorted-array.js
+++ Leetcode/ArrayAndString/26-remove-duplicates-from-sorted-array.js
@@ -4,26 +4,24 @@
  */
 var removeDuplicates = function(nums) {
 
-    let k = nums.length;
+    /**
+    k는 목표 배열의 원소 개수이자, 겹치지 않는 새로운 수를 추가하기 위한 인덱스 번호이다.
 
-    let i = 1;
-    while (i < nums.length && nums[i] !== '_') {
-        if (nums[i - 1] === nums[i]) {
-            moveLeft(i);
-            k -= 1;
-        } else {
-            i += 1;
+    nums는 이미 정렬되어 있으므로, 
+    반복문을 통한 현재 항과 직전 항의 원소값이 다른 경우, 새로운 원소 값을 발견한 것으로 간주하고,
+    nums[k]에 그 새로운 값을 복사하여 넣는다.
+
+    k는 i보다 작거나 같으므로 앞으로 탐색할 자료를 해치지 않는다.
+     */
+
+    let k = 1;  // index for new non-duplicating number.
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i - 1] !== nums[i]) {
+            nums[k] = nums[i];
+            k += 1;
         }
-
     }
 
     return k;
-
-    function moveLeft(removeIdx) {
-        for (let i = removeIdx; i < nums.length - 1; i++) {
-            nums[i] = nums[i + 1];
-        }
-        nums[nums.length - 1] = '_';
-    }
-
 };
